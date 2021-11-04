3a33cf660de846e5b40fbb4f7326b6724404b587
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-11-04 02:44:49 +0000

    Exercise/B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,12 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+    let content = document.querySelector("#content");
+    let list = document.createElement("ul");
+    content.appendChild(list);
+    arrayOfPeople.forEach((shoppingItem) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = shoppingItem;
+    list.appendChild(listItem);
+  });
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
