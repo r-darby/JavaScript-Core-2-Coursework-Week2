0e15377156176d4a6528def018ee0c5ce4aa1f97
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-11-04 04:40:22 +0000

    /*

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,5 +1,32 @@
 function readingList(books) {
-  // Write your code here...
+  let contentId = document.querySelector("#content");
+  //contentId.style.display = "flex";
+  let bookListHeading1 = document.createElement("h1");
+  bookListHeading1.innerText = "Book List";
+  let unorderedList = document.createElement("ul");
+  unorderedList.className = "ul";
+  contentId.appendChild(bookListHeading1);
+  contentId.appendChild(unorderedList);
+  books.forEach((book) => {
+    let newParagraph = document.createElement("p");
+    //newParagraph.style.display = "inline-block";
+    let list = document.createElement("li");
+    let imageEl = document.createElement("img");
+    imageEl.src = book.coverPage.src;
+    imageEl.alt = book.coverPage.alt;
+    imageEl.style.width = "auto";
+    newParagraph.innerText = book.title + " - " + book.author;
+    if (book.alreadyRead){
+      list.style.backgroundColor = "green";
+    }else{
+      list.style.backgroundColor = "red";
+    }
+    list.appendChild(newParagraph);
+    list.appendChild(imageEl);
+    list.className = "li";
+    unorderedList.appendChild(list);
+    //contentId.appendChild(newParagraph)
+  });
 }
 
 const books = [
@@ -7,7 +34,7 @@ const books = [
     title: "The Design of Everyday Things",
     author: "Don Norman",
     alreadyRead: false
-  },
+    },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
