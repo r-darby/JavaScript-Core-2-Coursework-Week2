6968b567f609f248be6ec3d374360a3de0e03aa1
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-11-04 05:57:34 +0000

    /*

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,31 +1,6 @@
 function readingList(books) {
   let contentId = document.querySelector("#content");
-  //contentId.style.display = "flex";
-  let bookListHeading1 = document.createElement("h1");
-  bookListHeading1.innerText = "Book List";
-  let unorderedList = document.createElement("ul");
-  unorderedList.className = "ul";
-  contentId.appendChild(bookListHeading1);
-  contentId.appendChild(unorderedList);
-  books.forEach((book) => {
-    let newParagraph = document.createElement("p");
-    //newParagraph.style.display = "inline-block";
-    let list = document.createElement("li");
-    let imageEl = document.createElement("img");
-    imageEl.src = book.coverPage.src;
-    imageEl.alt = book.coverPage.alt;
-    imageEl.style.width = "auto";
-    newParagraph.innerText = book.title + " - " + book.author;
-    if (book.alreadyRead){
-      list.style.backgroundColor = "green";
-    }else{
-      list.style.backgroundColor = "red";
-    }
-    list.appendChild(newParagraph);
-    list.appendChild(imageEl);
-    list.className = "li";
-    unorderedList.appendChild(list);
-    //contentId.appendChild(newParagraph)
+  
   });
 }
 
