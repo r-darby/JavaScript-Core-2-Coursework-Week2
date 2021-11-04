9010ae6478ca34c8c30cc45947683eb0ae51d28d
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-11-04 02:11:37 +0000

    Update script.js

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach(person => {
+	  let name = document.createElement("h1");
+    content.appendChild(name);
+    name.innerText = person.name;
+    let job = document.createElement("h2");
+    content.appendChild(job);
+    job.innerText = person.job;
+  });
+}
+
 }
 
 let people = [
@@ -8,4 +18,5 @@ let people = [
   { name: "Boris", job: "Prime Minister" },
 ];
 
+console.log(listOfNames)
 listOfNames(people);
